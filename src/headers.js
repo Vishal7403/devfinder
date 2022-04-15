4c1eea6d50ef1d2983773e3f69777e28ca6c53a4
vishal <vvish4me@gmail.com>
2022-04-15 15:05:39 +0530

    adding auth key
    Signed-off-by: vishal <vvish4me@gmail.com>

--- src/headers.js
+++ src/headers.js
@@ -1,6 +1,6 @@
 const headers={
     "Content-type":"application/json",
     'Accept': 'application/vnd.github.v3.raw',
-    'Authorization': `token ${process.env.REACT_APP_TOKEN}`          
+    'Authorization': `token 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`          
   }
 module.exports={headers}
